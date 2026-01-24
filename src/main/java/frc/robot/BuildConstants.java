f2b75ac3ea4ce2a6bbf82f6aaf52334b10098741
GabrielKuz <gabrielkuzowsky2027@lschs.org>
2026-01-24 12:38:46 -0500

    copied paths and autos from main repo

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,14 +5,14 @@ package frc.robot;
  */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "protoSwerve";
+  public static final String MAVEN_NAME = "2026-ProtoSwerve";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 7;
-  public static final String GIT_SHA = "86c20a6cf086b062b104a692b81835074ccaa622";
-  public static final String GIT_DATE = "2026-01-17 20:56:38 EST";
+  public static final int GIT_REVISION = 8;
+  public static final String GIT_SHA = "4472b08b9774223b18435d5b6f96d02e34466893";
+  public static final String GIT_DATE = "2026-01-18 12:19:59 EST";
   public static final String GIT_BRANCH = "master";
-  public static final String BUILD_DATE = "2026-01-18 11:53:42 EST";
-  public static final long BUILD_UNIX_TIME = 1768755222600L;
+  public static final String BUILD_DATE = "2026-01-24 10:03:14 EST";
+  public static final long BUILD_UNIX_TIME = 1769266994140L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
